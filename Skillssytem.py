80e1478ae8cb596575e7a5428616fd7b74b4edaa
neonzz1 <treble.6@live.co.uk>
2023-06-19 22:49:52 +0100

    Update Skillssytem.py

--- Skillssytem.py
+++ Skillssytem.py
@@ -2,12 +2,21 @@ from Sprites import BaseSprite
 
 class Skills(BaseSprite):
     def __init__(self):
-        super().__init__()
+        image_path = "img/status_bar.png"
+        super().__init__(image_path)
         skills = []
-        self.avalible_skills = []
+        self.skillimage = None
+        self.avalible_skills = ["fireball", "Bolt", "energyblast", "deathball", "self destruct", "fireballv2", "fired"]
+        self.load_image(image_path)
     
-    def rendering(self):
-        ''
+    def rendering(self, surface):
+        if not self.hide:
+
+            for i, item in enumerate(self.avalible_skills):
+                text_surface = self.smallerfont.render(item, True, (255, 255, 255))  # Render the text
+                text_rect = text_surface.get_rect()
+                text_rect.topleft = (10, i * 30 + 10)  # Position the text
+                surface.blit(text_surface, text_rect)  # Blit the text onto the window
 
     def Buy_Skill(self, player, handler):
         if player.level >= 1 and handler.money >= 10:
@@ -24,4 +33,10 @@ class Skills(BaseSprite):
             self.avalible_skills.append("fireballv2")
             self.avalible_skills.append("")
             self.avalible_skills.append("fired")
+
+    def load_image(self, image_path):
+        try:
+            self.skillimage = self.pygame.image.load(image_path).convert_alpha()
+        except self.pygame.error as e:
+            print("Error loading inventory image:", str(e))
 
